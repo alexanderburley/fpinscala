30daaaee6d8a4f2d4d12ab16ec7be69a37776621
Alex Burley <alexburley3@gmail.com>
2020-04-20 20:42:07 +0100

    Ex 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -5,13 +5,39 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def size[A](t: Tree[A]): Int = t match {
-    case Leaf(_)      => 1
-    case Branch(l, r) => 1 + size(l) + size(r)
-  }
 
-  def maximum(t: Tree[Int]): Int = t match {
-    case Leaf(v)      => v
-    case Branch(l, r) => maximum(l) max maximum(r)
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(n)      => f(n)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+
   }
+  // def size[A](t: Tree[A]): Int = t match {
+  //   case Leaf(_)      => 1 // A => B
+  //   case Branch(l, r) => 1 + size(l) + size(r) Tree[A], Tree[A] => 1 +
+  // }
+
+  def size[A](t: Tree[A]): Int =
+    fold(t) { n => 1 } { (l, r) => 1 + l + r }
+
+  // def maximum(t: Tree[Int]): Int = t match {
+  //   case Leaf(v)      => v
+  //   case Branch(l, r) => maximum(l) max maximum(r)
+  // }
+
+  def maximum(t: Tree[Int]): Int = fold(t)(n => n)((l, r) => l max r)
+
+  // def depth(t: Tree[Int]): Int = t match {
+  //   case Leaf(_)      => 1
+  //   case Branch(l, r) => (1 + depth(l)) max (1 + depth(r))
+  // }
+
+  def depth(t: Tree[Int]): Int = fold(t)(n => 1)((l, r) => (1 + l) max (1 + r))
+
+  // def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+  //   case Leaf(x)      => Leaf(f(x))
+  //   case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  // }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(n => Leaf(f(n)): Tree[B])(Branch(_, _))
 }
--- exercises/src/test/scala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/datastructures/TreeSpec.scala
@@ -3,8 +3,9 @@ import fpinscala.datastructures.Tree._
 import fpinscala.datastructures._
 
 class TreeSpec extends FlatSpec {
-  val t = Branch(Leaf(1), Leaf(2))
-  val tLarge = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+  val l = Leaf(1)
+  val t = Branch(l, Leaf(2))
+  val tLarge = Branch(Branch(l, Leaf(2)), Branch(Leaf(3), Leaf(4)))
 
   "size" should "count the number of nodes in a small tree" in {
     assert(size(t) == 3)
@@ -15,7 +16,7 @@ class TreeSpec extends FlatSpec {
   }
 
   it should "count a singe leaf" in {
-    assert(size(Leaf(1)) == 1)
+    assert(size(l) == 1)
   }
 
   "maximum" should "return 2 for t" in {
@@ -27,6 +28,30 @@ class TreeSpec extends FlatSpec {
   }
 
   it should "return 1 for 1 leaf" in {
-    assert(maximum(Leaf(1)) == 1)
+    assert(maximum(l) == 1)
+  }
+
+  "depth" should "return 1 for depth 1" in {
+    assert(depth(l) == 1)
+  }
+
+  it should "return 2 for t" in {
+    assert(depth(t) == 2)
+  }
+
+  it should "return 3 for t" in {
+    assert(depth(tLarge) == 3)
+  }
+
+  "map" should "modifies a leaf" in {
+    assert(map(l){n: Int => n + 1}.toString == Leaf(2).toString)
+  }
+
+  it should "modify a tree" in {
+    assert(map(t){n: Int => n + 1}.toString == Branch(Leaf(2), Leaf(3)).toString)
+  }
+
+    it should "modify a tree of ints to string" in {
+    assert(map(t){n: Int => n.toString}.toString == Branch(Leaf("1"), Leaf("2")).toString)
   }
 }
