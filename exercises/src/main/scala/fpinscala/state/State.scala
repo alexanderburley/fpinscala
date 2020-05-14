31762232715df2d87970980e209fb656345e9782
Alex Burley <alexburley3@gmail.com>
2020-05-14 10:09:44 +0100

    Ex6.10

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -134,41 +134,47 @@ object RNG {
     }
   }
 
-  // def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
-  //   rng => {
-  //     val (a, rng2) = s(rng)
-  //     (f(a), rng2)
-  //   }
-
-  // def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-  //   rng => {
-  //     val (a, rngA) = ra(rng)
-  //     val (b, rngB) = rb(rng)
-  //     (f(a, b), rngA)
-  //   }
-
   def mapFM[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(x => unit(f(x)))
-
-  // def map2FM[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-  //   flatMap { rng =>
-  //     {
-  //       val (a, rngA) = ra(rng)
-  //       val (b, rngB) = rb(rngA)
-  //       ((a, b), rngB)
-  //     }
-  //   } { (t) => unit(f(t._1, t._2)) }
 
   def map2FM[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     flatMap(ra)(a => map(rb)(b => f(a, b)))
 }
 
+// case class StateAnswer[S, +A](run: S => (A, S)) {
+//   def map[B](f: A => B): State[S, B] =
+//     flatMap(a => unit(f(a)))
+//   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+//     flatMap(a => sb.map(b => f(a, b)))
+//   def flatMap[B](f: A => State[S, B]): State[S, B] =
+//     State(s => {
+//       val (a, s1) = run(s)
+//       f(a).run(s1)
+//     })
+// }
+
 case class State[S, +A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    ???
+  def map[B](f: A => B): State[S, B] = flatMap(x => State(sx => (f(x), sx)))
+
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    flatMap(a => sb.map(b => f(a, b)))
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    State(s => {
+      val (x, ns) = this.run(s)
+      f(x).run(ns)
+    })
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+    State(s =>
+      fs.foldRight((List[A](), s)) { (v, acc) =>
+        {
+          val (accList, currRng) = acc
+          val (generatedValue, nextRng) = v.run(currRng)
+          (generatedValue :: accList, nextRng)
+        }
+      }
+    )
+
 }
 
 sealed trait Input
@@ -180,4 +186,35 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  // The idiomatic solution is expressed via foldRight
+  def sequenceViaFoldRight[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+  // This implementation uses a loop internally and is the same recursion
+  // pattern as a left fold. It is quite common with left folds to build
+  // up a list in reverse order, then reverse it at the end.
+  // (We could also use a collection.mutable.ListBuffer internally.)
+  def sequence1[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S, A]], acc: List[A]): (List[A], S) =
+      actions match {
+        case Nil    => (acc.reverse, s)
+        case h :: t => h.run(s) match { case (a, s2) => go(s2, t, a :: acc) }
+      }
+    State((s: S) => go(s, sas, List()))
+  }
+
+  // We can also write the loop using a left fold. This is tail recursive like the
+  // previous solution, but it reverses the list _before_ folding it instead of after.
+  // You might think that this is slower than the `foldRight` solution since it
+  // walks over the list twice, but it's actually faster! The `foldRight` solution
+  // technically has to also walk the list twice, since it has to unravel the call
+  // stack, not being tail recursive. And the call stack will be as tall as the list
+  // is long.
+  def sequenceViaFoldLeft[S, A](l: List[State[S, A]]): State[S, List[A]] =
+    l.reverse.foldLeft(unit[S, List[A]](List()))((acc, f) =>
+      f.map2(acc)(_ :: _)
+    )
 }
