cdc54a286acf5190dc48e8b1ea1197237ebf506e
Alex Burley <alexburley3@gmail.com>
2020-06-23 09:30:30 +0100

    Ex8.1 Ex8,2

---
+++ exercises/src/test/scala/fpinscala/testing/PropTest.scala
@@ -0,0 +1,41 @@
+package fpinscala.testing
+
+class PropTest extends org.scalatest.FlatSpec {}
+
+/*
+
+Exercise 8.1
+
+sum: List[Int] => Int - Properties
+
+- Reversing a list and summing it should give the same result as an original list
+- If all list elements are same value, sum should equal length element * N
+- Sum of an empty list should be 0
+
+Answer
+
+• The sum of the empty list is 0.
+• The sum of a list whose elements are all equal to x is just the list’s length multiplied by x. We
+might express this as sum(List.fill(n)(x)) == n*x
+• For any list, l, sum(l) == sum(l.reverse), since addition is commutative.
+• Given a list, List(x,y,z,p,q), sum(List(x,y,z,p,q)) == sum(List(x,y)) + sum(List(z,p,q)),
+since addition is associative. More generally, we can partition a list into two subsequences
+whose sum is equal to the sum of the overall list.
+• The sum of 1,2,3…n is n*(n+1)/2.
+
+Exercise 8.2
+
+max: List[Int] => Int - Properties
+
+- Reversing a list should have no consequence
+- If all list elements are the same, sum should be value N
+- Max of an empty list should throw an error
+
+Answer
+
+• The max of a single element list is equal to that element.
+• The max of a list is greater than or equal to all elements of the list.
+• The max of a list is an element of that list.
+• The max of the empty list is unspecified and should throw an error or return None.
+
+ */
